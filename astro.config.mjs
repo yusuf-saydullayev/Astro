41d8cd2902096299536afdeb0903875460626e15
Yusuf <yusufsaydullayev@gmail.com>
2022-09-24 14:54:35 +0500

    New: vite pwa

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import { VitePWA } from 'vite-plugin-pwa';
 import react from "@astrojs/react";
 import image from "@astrojs/image";
 import netlify from "@astrojs/netlify/functions";
@@ -9,7 +10,7 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site:'https://astro-poux.netlify.app/',
-  integrations: [tailwind(), react(), image(), sitemap()],
+  integrations: [tailwind(), react(), image(), sitemap(),VitePWA()],
   output: "server",
   adapter: netlify()
 });
