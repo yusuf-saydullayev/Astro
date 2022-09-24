ebd7450f46783aa0d81e4b49d32349304eac650e
Yusuf <yusufsaydullayev@gmail.com>
2022-09-24 14:28:48 +0500

    New: astro sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,14 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import image from "@astrojs/image";
-
 import netlify from "@astrojs/netlify/functions";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), image()],
+  site:'https://astro-poux.netlify.app/',
+  integrations: [tailwind(), react(), image(), sitemap()],
   output: "server",
   adapter: netlify()
 });
