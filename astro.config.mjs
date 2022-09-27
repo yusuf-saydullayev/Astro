f3780bccc2630bfeb29c44e7944828c8e451f62f
Yusuf <yusufsaydullayev@gmail.com>
2022-09-27 21:32:14 +0500

    Fix: manifest

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,30 +6,9 @@ import image from "@astrojs/image";
 import netlify from "@astrojs/netlify/functions";
 import { VitePWA } from 'vite-plugin-pwa';
 
-import { manifest, seoConfig } from "./utils/seoConfig"
-
-
-// https://astro.build/config
 export default defineConfig({
   site:'https://astro-poux.netlify.app/',
-  integrations: [tailwind(), react(), image(), sitemap()],
-  vite: {
-		plugins: [
-			VitePWA({
-				registerType: "autoUpdate",
-				manifest,
-				workbox: {
-				  globDirectory: 'dist',
-				  globPatterns: [
-				    '**/*.{js,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico}',
-				  ],
-				  // Don't fallback on document based (e.g. `/some-page`) requests
-				  // This removes an errant console.log message from showing up.
-				  navigateFallback: null,
-				},
-			})
-		]
-	},
+  integrations: [tailwind(), react(), image(), sitemap(),VitePWA()],
   output: "server",
   adapter: netlify()
 });
