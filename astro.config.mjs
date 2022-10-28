db82d192973169739b1b77f4a6a2fd9473f96078
Yusuf <yusufsaydullayev@gmail.com>
2022-10-28 22:17:01 +0500

    img

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,19 @@
 import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
 import tailwind from '@astrojs/tailwind';
+
+// https://astro.build/config
 import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
 import react from '@astrojs/react';
+
+// https://astro.build/config
 import image from '@astrojs/image';
+
+// https://astro.build/config
 import netlify from '@astrojs/netlify/functions';
-
-// https://astro.build/config
-
-// https://astro.build/config
-
-// https://astro.build/config
-
-// https://astro.build/config
-
-// https://astro.build/config
-
-// https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
