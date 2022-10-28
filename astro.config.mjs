d9f97e5122244c39fa9b57596191311a86680be9
Yusuf <yusufsaydullayev@gmail.com>
2022-10-28 21:30:36 +0500

    some upgrades

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,25 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
-import react from "@astrojs/react";
-import image from "@astrojs/image";
-import netlify from "@astrojs/netlify/functions";
-import { VitePWA } from 'vite-plugin-pwa';
+import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
+import react from '@astrojs/react';
+import image from '@astrojs/image';
+// import netlify from '@astrojs/netlify/functions';
+import netlify from '@astrojs/netlify/edge-functions';
+
+
+// https://astro.build/config
+
+// https://astro.build/config
+
+// https://astro.build/config
+
+// https://astro.build/config
+
+// https://astro.build/config
 
 export default defineConfig({
-  site:'https://astro-poux.netlify.app/',
-  integrations: [tailwind(), react(), image(), sitemap(),VitePWA()],
-  output: "server",
+  site: 'https://astro-poux.netlify.app/',
+  integrations: [tailwind(), react(), image(), sitemap()],
+  output: 'server',
   adapter: netlify()
 });
