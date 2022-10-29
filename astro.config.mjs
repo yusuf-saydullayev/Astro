35df60a8a7b8f609a84d445f5358b0274c1a074b
Yusuf <yusufsaydullayev@gmail.com>
2022-10-29 23:17:19 +0500

    FIX: downgraded astro package version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,24 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-import tailwind from '@astrojs/tailwind';
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
-import sitemap from '@astrojs/sitemap';
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
-import react from '@astrojs/react';
+import react from '@astrojs/react'
 
 // https://astro.build/config
-import image from '@astrojs/image';
+import image from '@astrojs/image'
 
 // https://astro.build/config
-import netlify from '@astrojs/netlify/functions';
+import netlify from '@astrojs/netlify/functions'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astro-poux.netlify.app/',
-  integrations: [tailwind(), react(), image(), sitemap()],
+  integrations: [tailwind(), react(), image({ serviceEntryPoint: '@astrojs/image/sharp' }), sitemap()],
   output: 'server',
-  adapter: netlify()
-});
+  adapter: netlify(),
+})
