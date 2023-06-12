360e5b7d9a27c57aa749e30a643ab74ab18b93c6
yusuf-saydullayev <yusufsaydullayev@gmail.com>
2023-06-12 13:36:15 +0500

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,11 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 import tailwind from '@astrojs/tailwind';
-
-// https://astro.build/config
 import sitemap from '@astrojs/sitemap';
-
-// https://astro.build/config
 import react from '@astrojs/react';
-
-// https://astro.build/config
 import image from '@astrojs/image';
-
-// https://astro.build/config
 import netlify from '@astrojs/netlify/functions';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://astro-poux.netlify.app/',
   integrations: [tailwind(), react(), image({
